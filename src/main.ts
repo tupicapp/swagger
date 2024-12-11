0f6ee2a2db0d13e760ea3ccb99d0fa0572e97e47
Mohammad Alimardani <alimardani94@gmail.com>
2024-12-12 02:20:20 +0330

    get oauth config from core

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,16 @@ import './css/swagger.css';
 import {SwaggerUIBundle, SwaggerUIStandalonePreset} from "swagger-ui-dist"
 
 const coreUrl = import.meta.env.VITE_CORE_URL;
+const oauthUrl = import.meta.env.VITE_OAUTH_URL;
+const blogUrl = import.meta.env.VITE_BLOG_URL ?? "/configs/blog-v1.json";
 
 SwaggerUIBundle({
     dom_id: '#swagger-ui',
     urls: [
+        {url: oauthUrl, name: "OAuth"},
         {url: coreUrl, name: "Core (v2.1)"},
         {url: "/configs/core-v2.json", name: "Core (v2)"},
-        {url: "/configs/blog-v1.json", name: "Blog (v1)"},
-        {url: "/configs/oauth-server.json", name: "OAuth"},
+        {url: blogUrl, name: "Blog (v1)"},
     ],
     "urls.primaryName": "Core (v2.1)",
     presets: [
