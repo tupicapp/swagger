793dc738eb60b369875410a6b3bd17f831fd9dc5
alimardani <alimardani94@gmail.com>
2026-03-08 21:48:10 +0330

    rename

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import './css/swagger.css';
 //js
 import {SwaggerUIBundle, SwaggerUIStandalonePreset} from "swagger-ui-dist"
 
-const coreUrl = import.meta.env.VITE_CORE_URL ||  "https://api.gibical.app/swagger/api.json";
-const oauthUrl = import.meta.env.VITE_OAUTH_URL || "https://api.gibical.app/swagger/oauth.json";
+const coreUrl = import.meta.env.VITE_CORE_URL ||  "https://api.tupic.com/swagger/api.json";
+const oauthUrl = import.meta.env.VITE_OAUTH_URL || "https://api.tupic.com/swagger/oauth.json";
 const blogUrl = import.meta.env.VITE_BLOG_URL || "/configs/blog-v1-development.json";
 
 SwaggerUIBundle({
